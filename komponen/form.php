32fef37539796ea2c3287074289d109a47b2bb7d
chandraauliatama <chandraaulia4@gmail.com>
2023-11-21 01:27:29 +0700

    update

--- komponen/form.php
+++ komponen/form.php
@@ -1,49 +1,28 @@
+<?php 
+function inputContainerComponent($type, $name, $label, $value) 
+{
+    return "
+        <div class='input-container'>
+        <label for='{$name}'>{$label}</label>
+        <input type='{$type}' name='{$name}' id='{$name}' value='{$value}'>
+        </div>
+    ";
+}
+
+?>
+
 <div>
     <input type="hidden" name="id"  value="<?= $row['id'] ?>">
-    <div class="input-container">
-        <label for="namadep">Nama Depan:</label>
-        <input type="text" name="namadep" id="namadep" value="<?= $row['namadep'] ?>">
-    </div>
 
-    <div class="input-container">
-        <label for="namabel">Nama Belakang:</label>
-        <input type="text" name="namabel" id=namabel  value="<?= $row['namabel'] ?>">
-    </div>
-
-    <div class="input-container">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username"  value="<?= $row['username'] ?>">
-    </div>
-
-    <div class="input-container">
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password"  value="<?= $row['password'] ?>">
-    </div>
-
-    <div class="input-container">
-        <label for="usia">Usia:</label>
-        <input type="text" name="usia" id="usia"  value="<?= $row['usia'] ?>">
-    </div>
-
-    <div class="input-container">
-        <label for="jk">Jenis Kelamin:</label>
-        <input type="text" name="jk" id="jk"  value="<?= $row['jk'] ?>">
-    </div>
-
-    <div class="input-container">
-        <label for="ttl">Tempat Tgl Lahir:</label>
-        <input type="text" name="ttl" id="ttl"  value="<?= $row['ttl'] ?>">
-    </div>
-
-    <div class="input-container">
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email"  value="<?= $row['email'] ?>">
-    </div>
-
-    <div class="input-container">
-        <label for="notel">No Telepon:</label>
-        <input type="tel" name="notel" id="notel"  value="<?= $row['notel'] ?>">
-    </div>
+    <?= inputContainerComponent('text', 'namadep', 'Nama Depan:', $row['namadep']) ?>
+    <?= inputContainerComponent('text', 'namabel', 'Nama Belakang:', $row['namabel']) ?>
+    <?= inputContainerComponent('text', 'username', 'Username:', $row['username']) ?>
+    <?= inputContainerComponent('password', 'password', 'Password:', $row['password']) ?>
+    <?= inputContainerComponent('number', 'usia', 'Usia:', $row['usia']) ?>
+    <?= inputContainerComponent('text', 'jk', 'Jenis Kelamin:', $row['jk']) ?>
+    <?= inputContainerComponent('text', 'ttl', 'TTL:', $row['ttl']) ?>
+    <?= inputContainerComponent('email', 'email', 'Email:', $row['email']) ?>
+    <?= inputContainerComponent('tel', 'notel', 'No Telepon:', $row['notel']) ?>
 
     <div class="input-container">
         <label ></label>
