785c0f4b7bd3c53851b3f8eb181d5400033c175b
chandraauliatama <chandraaulia4@gmail.com>
2023-11-21 00:23:41 +0700

    update

--- koneksi.php
+++ koneksi.php
@@ -1,25 +1,37 @@
 <?php
 
-if(!$_GET['module'] || ! in_array($_GET['module'], ['delete', 'create', 'update', 'view', 'search', 'edit']))
+function redirectJs($module = false)
 {
-    echo "<script>window.location.href='index.php'</script>";
+    return $module 
+        ? "window.location.href='index.php?module={$module}'" 
+        : "window.location.href='index.php'";
 }
 
+$allowedModule = ['delete', 'create', 'update', 'view', 'search', 'edit'];
+
+if(!$_GET['module'] || ! in_array($_GET['module'], $allowedModule))
+{
+    $redirect = redirectJs();
+
+    echo "<script>{$redirect}</script>";
+}
+
+// MySql Credentials 
 $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "066_chandra";
 
+// Create New MySQL connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);}
 
 function selectAllData($username = '')
 {
-    $select = $username ? "SELECT * FROM register WHERE username like '%{$username}%';" : "SELECT * FROM register;";
+    $select = $username 
+        ? "SELECT * FROM register WHERE username like '%{$username}%';" 
+        : "SELECT * FROM register;";
 
     global $conn;
 
@@ -41,44 +53,41 @@ function selectById($dataId)
     }
 }
 
-function deleteById($dataId)
-{
-    global $conn;
-    $deleteScript = "DELETE from register where id={$dataId};";
-    
-    return $conn->query($deleteScript) 
-        ? "<script>alert('Data Berhasil Dihapus'); window.location.href='index.php?module=view'</script>" 
-        : "Error: {$deleteScript} <br> $conn->error";
-}
-
 // Delete when koneksi accesed
 if($_GET['module'] == 'delete') {
-    echo deleteById($_GET['id']);
+    $deleteQuery = "DELETE from register where id={$_GET['id']};";
+
+    $redirect = redirectJs('view');
+
+    echo $conn->query($deleteQuery) === TRUE
+        ? "<script>alert('Data Berhasil Dihapus'); {$redirect} </script>" 
+        : "Error: {$deleteQuery} <br> $conn->error";
 }
 
 // Add new data 
 if($_GET['module'] == 'create') {
-    $mysql = "INSERT INTO register (namadep, namabel, username, password, usia, jk, ttl, email, notel) VALUES ('" 
-    . $_POST['namadep'] ."','"
-    . $_POST['namabel'] ."','"
-    . $_POST['username'] ."','"
-    . $_POST['password'] ."','"
-    . $_POST['usia'] ."','"
-    . $_POST['jk'] ."','"
-    . $_POST['ttl']."','"
-    . $_POST['email']."','"
-    . $_POST['notel']. "');";
+    $insertDataQuery = "INSERT INTO register (namadep, namabel, username, password, usia, jk, ttl, email, notel) VALUES (
+        '{$_POST['namadep']}', 
+        '{$_POST['namabel']}', 
+        '{$_POST['username']}', 
+        '{$_POST['password']}', 
+        '{$_POST['usia']}', 
+        '{$_POST['jk']}', 
+        '{$_POST['ttl']}', 
+        '{$_POST['email']}', 
+        '{$_POST['notel']}');
+    ";
 
-    if ($conn->query($mysql) === TRUE) {
-        echo "<script>alert('Data Berhasil Ditambahkan'); window.location.href='index.php'</script>";
-      } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+    $redirect = redirectJs();
+
+    echo $conn->query($insertDataQuery) === TRUE
+        ? "<script>alert('Data Berhasil Ditambahkan'); {$redirect} </script>"
+        : "Error: " . $insertDataQuery . "<br>" . $conn->error;
 }
 
 // update data
 if($_GET['module'] == 'update') {
-    $mysql = "UPDATE register SET 
+    $updateQuery = "UPDATE register SET 
         namadep='{$_POST['namadep']}', 
         namabel='{$_POST['namabel']}', 
         username='{$_POST['username']}', 
@@ -89,9 +98,9 @@ if($_GET['module'] == 'update') {
         jk='{$_POST['jk']}', 
         notel='{$_POST['notel']}' 
         where id={$_POST['id']}
-        ";
+    ";
 
-    echo $conn->query($mysql) === TRUE 
+    echo $conn->query($updateQuery) === TRUE 
       ? "<script>alert('Data Berhasil Diubah'); window.location.href='index.php?module=view'</script>" 
-      : "Error: {$sql} <br> {$conn->error}";
+      : "Error: {$updateQuery} <br> {$conn->error}";
 }
